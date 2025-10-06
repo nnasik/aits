6eb922567314e57f754454bc12f0119ced13f087
Nasik Navaz <snasik@live.com>
2025-10-06 10:24:48 +0530

    Deployment Scripts

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use App\Models\User;
 
 
 class RolesTableSeeder extends Seeder
