f97c3d951806b08c119b117a7ea6f3bd1d636afa
Nasik Navaz <snasik@live.com>
2026-01-29 14:59:42 +0530

    Chnage trainee

--- resources/views/job/modals/edit_trainee.blade.php
+++ resources/views/job/modals/edit_trainee.blade.php
@@ -22,12 +22,12 @@
 
           <div class="col-md-6">
             <label class="form-label">Company Name</label>
-            <input type="text" class="form-control bg-light" id="company_name_in_certificate" readonly>
+            <input type="text" class="form-control bg-light" id="company_name_in_certificate">
           </div>
 
           <div class="col-md-6">
             <label class="form-label">Course Name</label>
-            <input type="text" class="form-control bg-light" id="course_name_in_certificate" readonly>
+            <input type="text" class="form-control bg-light" id="course_name_in_certificate">
           </div>
 
           <div class="col-md-6">
