739f8f53d8b67ae0e74187694d23a351a0c26065
Nashath Nasik <snasik@live.com>
2026-08-20 11:16:16 +0400

    Signature update 7

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -283,7 +283,7 @@ class CertificateController extends Controller
 
         // Signature Option
         if (isset($user_settings['certificate_signature_option']) && $user_settings['certificate_signature_option']==1) {
-             $pdf->Image(public_path('assets/images/digital/digital_signature_1-min.png'), 130, 135, 22, 18);
+             $pdf->Image(public_path('assets/images/digital/digital_signature_1-min.png'), 140, 135, 22, 18);
         }
 
         // Signature Option 2
